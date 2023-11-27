339374f2ad63b08602fbaaee2ae1067ea1516a5b
RenanMM <renancemm@gmail.com>
2023-11-27 18:40:21 -0300

    Script da página product realocado para o script.js

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+// Product Page
+
+const mainImage = document.getElementById("main-image")
+const smallImage = document.getElementsByClassName("small-image")
+
+smallImage[0].onclick = function () {
+    mainImage.src = smallImage[0].src
+}
+smallImage[1].onclick = function () {
+    mainImage.src = smallImage[1].src
+}
+smallImage[2].onclick = function () {
+    mainImage.src = smallImage[2].src
+}
+smallImage[3].onclick = function () {
+    mainImage.src = smallImage[3].src
+}
